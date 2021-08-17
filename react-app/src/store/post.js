7d89b3bd16f74a07ef8e470f563bc61a9446b7a2
sergeveli <70241892+sergeveli@users.noreply.github.com>
2021-08-17 17:27:46 -0400

    ReactPostDeleteButton-In Progress

--- react-app/src/store/post.js
+++ react-app/src/store/post.js
@@ -1,9 +1,15 @@
 const ADD_POST = 'posts/ADD_POST'
+const DELETE_POST = 'posts/DELETE_POST'
 
 const addPost = (post) => ({
     type: ADD_POST,
     payload: post
 });
+
+const deletePost = (post) => ({
+  type: DELETE_POST,
+  payload: post
+})
 
 const initialState = {
     post: null
@@ -33,15 +39,42 @@ export const createPost = (post) => async (dispatch) => {
       } else {
         return ['An error occurred. Please try again.']
       }
-
-
-
 }
+
+export const erasePost = (post) => async (dispatch) => {
+  const response = await fetch(`/api/posts/${post.id}/delete`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      post
+    })
+
+  })
+
+  if (response.ok) {
+    const data = await response.json();
+    dispatch(deletePost(data))
+    return null;
+  } else if (response.status < 500) {
+    const data = await response.json();
+    if (data.errors) {
+      return data.errors;
+    }
+  } else {
+    return ['An error occurred. Please try again.']
+  }
+}
+
 
 export default function reducer(state = initialState, action) {
     switch (action.type) {
       case ADD_POST:
         return { posts: action.payload }
+      case DELETE_POST:
+        delete state[action.payload.id]
+        return state
       default:
         return state;
     }
