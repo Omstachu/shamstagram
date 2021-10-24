b164990cf677184199d6ea058309f244b0d34d9a
Brandon <omstachu626@gmail.com>
2021-10-23 21:30:05 -0700

    comments are being added to database

--- app/api/comment_routes.py
+++ app/api/comment_routes.py
@@ -2,7 +2,7 @@ from flask import Blueprint, request, jsonify
 import json
 from flask_login import login_required, current_user
 from app.api.auth_routes import login
-from app.models import User, Post, Image, db
+from app.models import Comment, db
 from ..forms import CommentForm
 
 comment_routes = Blueprint("comments", __name__)
@@ -11,15 +11,23 @@ comment_routes = Blueprint("comments", __name__)
 @comment_routes.route('/', methods=["POST"])
 @login_required
 def new_comment():
-    form = CommentForm()
-    # print(dir(request))
-    print("eggs", request.form["content"])
-    # print("form", form.data)
-    # comment_data = json.loads(request.form)
-    # comment_data = request.form["new_comment"]
-    # comment_data = json.loads(request.form["new_comment"])
-    # print("comment_data", comment_data)
-    # print(json.loads(request.form["new_comment"]))
-    # print(comment_data)
+    comment = Comment(
+        userId=request.form["userId"],
+        postId=request.form["postId"],
+        content=request.form["content"]
+    )
 
-    return "nana"
+    db.session.add(comment)
+    db.session.flush()
+    db.session.refresh(comment)
+    db.session.commit()
+
+    comment = {
+        "id": comment.id,
+        "userId": comment.userId,
+        "postId": comment.postId,
+        "content": comment.content
+    }
+
+    print(comment)
+    return comment
